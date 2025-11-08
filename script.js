15fc512ba959d4ec42cfcab6886a191ee04d125c
skrajb <59959974+skrajb@users.noreply.github.com>
2025-11-08 12:54:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -853,6 +853,10 @@ function saveResultToSheet() {
 // ✅ Attach automatic call when results are shown or submitted
 document.addEventListener("DOMContentLoaded", () => {
     const submitBtn = document.getElementById("submit-btn");
+//Hide User-info on Submit
+	 const userInfo = document.getElementById("user-info");
+  if (userInfo) userInfo.style.display = "none";
+	
     if (submitBtn) {
         submitBtn.addEventListener("click", () => {
             // Save after the result table is populated
